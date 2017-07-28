3601eafc67d454f0dfeec5c52bd0d1f76b5f7f52
Deobrat <deobrat811@gmail.com>
2017-07-28 05:49:58 -0700

    commit

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -10,6 +10,8 @@ import { FormsModule } from '@angular/forms';
 
 export class QuizComponent {
     questions: any[];
+    questionnum:number=1;
+    marks:number=0;
     question: any;
     quesitr:number=0;
     start:boolean=true;
@@ -23,13 +25,19 @@ export class QuizComponent {
     nextQuestion() {
 
          this.quesitr=this.quesitr+1;
-         console.log(this.option);
+          if(this.option==this.question.answer)
+          {
+             this.marks=this.marks+1;
+             console.log(this.marks);
+          }
          this.question=this.questions[this.quesitr];
+         this.questionnum=this.questionnum+1;
     }
     startQuiz(){
         this.question=this.questions[this.quesitr];
         this.start=false;
     }
+
 
 
 }

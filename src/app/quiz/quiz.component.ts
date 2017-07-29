da87e4a76b6f84a12cec2244bf497ffff81f713d
Deobrat <deobrat811@gmail.com>
2017-07-29 12:06:03 -0700

    commit

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -10,12 +10,17 @@ import { FormsModule } from '@angular/forms';
 
 export class QuizComponent {
     questions: any[];
-    questionnum:number=1;
-    marks:number=0;
+    questionnum: number = 1;
+    marks: number = 0;
     question: any;
-    quesitr:number=0;
-    start:boolean=true;
-    option:string;
+    quesitr: number = 0;
+    start: boolean = true;
+    option: string;
+    length: number;
+    indexlist: number[] = [];
+    index: number;
+    suffledques:any[];
+    item:any;
     constructor(private quizserObj: QuizService) {
 
     }
@@ -23,21 +28,31 @@ export class QuizComponent {
         this.quizserObj.getQuestion().subscribe(questions => this.questions = questions);
     }
     nextQuestion() {
+        this.indexlist.push(this.index);
+        this.quesitr = this.quesitr + 1;
 
-         this.quesitr=this.quesitr+1;
-          if(this.option==this.question.answer)
-          {
-             this.marks=this.marks+1;
-             console.log(this.marks);
-          }
-         this.question=this.questions[this.quesitr];
-         this.questionnum=this.questionnum+1;
+        if (this.option == this.question.answer) {
+            this.marks = this.marks + 1;
+        }
+        this.question = this.questions[this.quesitr];
+        this.questionnum = this.questionnum + 1;
     }
-    startQuiz(){
-        this.question=this.questions[this.quesitr];
-        this.start=false;
+    startQuiz() {
+        this.suffle();
+        this.question = this.questions[this.quesitr];
+        this.start = false;
     }
 
+    suffle() {
+        this.length = this.questions.length;
+        for (let i = this.length - 1; i >= 0; i--) {
+               this.index=Math.floor(Math.random()*(i+1)); 
+               this.item=this.questions[this.index];
+               this.questions[this.index]=this.questions[i];
+               this.questions[i]=this.item;
+
+        }
+    }
 
 
 }

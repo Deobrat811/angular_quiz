b7f8b83efd408acb16a47b7669a4364fa0a045ee
Deobrat <deobrat811@gmail.com>
2017-12-05 13:18:55 +0530

    Update result.component.ts
    update with chart feature

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -15,11 +15,28 @@ export class ResultComponent {
     total_ques:number;
     pass:number=0.6;
     result:string;
+    private _total:number =0;
+
+    items:Array<{name:string,count:number,color:string}>=[
+        {name:'Orange',count:0,color:'#2bc65a'},
+        {name:'Apple',count:this.total_ques-this.marksObt,color:'red'},
+        {name:'Pear',count:0,color:'green'}
+      ];
+   
+    
     
     constructor(route: ActivatedRoute,quizservice:QuizService) {
         this.marks=quizservice.getMarks();
         this.marksObt=this.marks.obtMarks;
         this.total_ques=this.marks.totalQues;
+
+        if(this.items.length>0)
+            {
+
+              this.items[0].count=this.marksObt;
+              this.items[1].count=this.total_ques-this.marksObt;
+              this._total = this.items.map(a=>a.count).reduce((x,y)=>x+y);
+            }
     }
     ngOnInit(){
         if(this.marksObt/this.total_ques>=0.5){
@@ -30,6 +47,26 @@ export class ResultComponent {
         }
         
     }
+      getPerimeter(radius:number):number
+      {
+        return Math.PI*2*radius;
+      }
+    
+      getColor(index:number):string
+      {
+        return this.items[index].color;
+      }
+    
+      getOffset(radius:number,index:number):number
+      {   
+        var percent=0;
+        for(var i=0;i<index;i++)
+        {
+          percent+=((this.items[i].count)/this._total);
+        }
+        var perimeter = Math.PI*2*radius;
+        return perimeter*percent;
+      }
 }
 
 
